7ee8cd5cfc08c177479d479361f3985557084d2f
louieogrady <l.ogrady@gmail.com>
2019-04-01 21:27:42 +0100

    fin

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+
+function greet(timeS) {
+  const x = parseInt(timeS, ); // not sure about the 2nd argument - parseInt converts strings to numbers?
+  if (x < 12 ) return 'Good Morning'
+  if (x > 12 && x < 17) return "Good Afternoon"
+  if (x > 5) return "Good Evening"
+}
+
+function displayMessage(display) {
+  document.getElementById('greeting').textContent = display
+}
+
+
+
+
+
+//
+// function greet() {
+
+//   }
+// }
+
+
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
