e19fa265c328d890d3f93484b3b3f9e1f3757fc3
LondonBishop <londonbishop@codecutters.co.uk>
2019-04-01 16:32:41 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeInput) {
+
+  let newTime = timeInput.split(':')[0];
+
+  if (newTime < 12 ) {
+      return "Good Morning";
+  } else {
+      if (newTime >= 12 && newTime <= 17) {
+        return "Good Afternoon";
+      } else {
+            if (newTime > 17){
+              return "Good Evening";
+            }
+        }
+    }
+  }
 /* Write your implementation of displayMessage() */
+function displayMessage(strMessage) {
+    document.getElementById("greeting").textContent = strMessage;
+}
