2e55187d7007f19b0096b9d03dd83e1356287232
khang <khang.le27@gmail.com>
2019-04-02 08:59:59 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+/* It would be a good idea to make sure hour isn't less than 0 or greater
+ * than 24...perhaps "throwing an Error" if one of these impossible cases
+ * happens.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Throwing_a_generic_error */
+
+function greet(timeString){
+  time = parseFloat(timeString.split())
+  if(time < 12){
+    return 'Good Morning'
+  }
+  else if(time  > 12 && time < 17){
+    return 'Good Afternoon'
+  }
+  else{
+    return 'Good Evening'
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  document.querySelector("h1#greeting").textContent = string;
+}
